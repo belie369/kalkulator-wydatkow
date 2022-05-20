42a42a5de7d04dc594aeface92640be05752d3e1
belie369 <katarzyna.derbich@gmail.com>
2022-05-20 11:38:59 +0200

    Added functions: showPanel, closePanel, checkForm, clearInputs with listeners

--- script.js
+++ script.js
@@ -21,3 +21,30 @@ let ID = 0;
 let categoryIcon;
 let selectedCategory;
 let moneyArr = [0];
+
+const showPanel = () => {
+	addTransactionPanel.style.display = 'flex';
+};
+
+const closePanel = () => {
+	addTransactionPanel.style.display = 'none';
+	clearInputs();
+};
+
+const checkForm = () => {
+	if (nameInput.value !== '' && amountInput.value !== '' && categorySelect.value !== 'none') {
+		console.log('ok');
+	} else {
+		alert('Wypełnij wszystkie pola!');
+	}
+};
+
+const clearInputs = () => {
+	nameInput.value = '';
+	amountInput.value = '';
+	categorySelect.selectedIndex = 0;
+};
+
+addTransactionBtn.addEventListener('click', showPanel);
+cancelBtn.addEventListener('click', closePanel);
+saveBtn.addEventListener('click', checkForm);
